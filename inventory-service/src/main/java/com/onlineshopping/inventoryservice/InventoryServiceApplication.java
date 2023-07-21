17867a3baf2302eecaebc5ab266d161a72807a38
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-21 10:06:20 +0530

    Added netflix eureka

--- inventory-service/src/main/java/com/onlineshopping/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/onlineshopping/inventoryservice/InventoryServiceApplication.java
@@ -5,6 +5,7 @@ import com.onlineshopping.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
